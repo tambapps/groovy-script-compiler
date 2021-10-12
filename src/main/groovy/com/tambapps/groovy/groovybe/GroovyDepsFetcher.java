2dad366e3fc35c0a2a624d37674a95137ed39e00
Tambapps <tambapps@gmail.com>
2021-10-12 09:12:40 +0200

    added maven dependency resolver jar

---
+++ src/main/groovy/com/tambapps/groovy/groovybe/GroovyDepsFetcher.java
@@ -0,0 +1,9 @@
+package com.tambapps.groovy.groovybe;
+
+// TODO use maven-dependency-resolver
+//  to fetch groovy jars
+
+
+// TODO handle <exclusions> tag in groovy jar
+public class GroovyDepsFetcher {
+}
