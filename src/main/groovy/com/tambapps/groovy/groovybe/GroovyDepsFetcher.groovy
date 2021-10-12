47fa9acc0501251b06361d61fbd7d41d2a97c0dd
Tambapps <tambapps@gmail.com>
2021-10-13 01:08:39 +0200

    code cleaning

--- src/main/groovy/com/tambapps/groovy/groovybe/GroovyDepsFetcher.groovy
+++ src/main/groovy/com/tambapps/groovy/groovybe/GroovyDepsFetcher.groovy
@@ -2,16 +2,12 @@ package com.tambapps.groovy.groovybe
 
 import com.tambapps.maven.dependency.resolver.DependencyResolver
 import com.tambapps.maven.dependency.resolver.data.DependencyResolvingResult
-import com.tambapps.maven.dependency.resolver.repository.RemoteMavenRepository
 import com.tambapps.maven.dependency.resolver.repository.RemoteSavingMavenRepository
 import com.tambapps.maven.dependency.resolver.version.FirstVersionFoundConflictResolver
 
 class GroovyDepsFetcher {
 
-  private static final File USER_HOME = new File(System.getProperty("user.home"))
-  private final RemoteSavingMavenRepository repository =
-      new RemoteSavingMavenRepository(new File(USER_HOME, ".m2"),
-          [new RemoteMavenRepository()])
+  private final RemoteSavingMavenRepository repository = new RemoteSavingMavenRepository()
   private final DependencyResolver resolver = new DependencyResolver(repository)
 
   // for now it only support groovy 3.X
